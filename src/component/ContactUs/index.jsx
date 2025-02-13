fba02faf2a389e871ee75428145c37d65bb904b2
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2025-02-13 12:30:13 +0530

    react router dpm

---
+++ src/component/ContactUs/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./hero.css"
+import Hero from './Hero'
+const ContactUs = () => {
+  return (
+    <div >
+          <Hero/>
+    </div>
+  )
+}
+
+export default ContactUs

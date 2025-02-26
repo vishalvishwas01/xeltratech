b51fe1527df18485f07bd639219249c4612ea53f
vishalvishwas01 <vishalvishwas7082@gmail.com>
2025-02-26 21:54:53 +0530

    jk

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,25 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./component/Homepage";
 import ContactUs from "./component/ContactUs/index";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import AboutUs from "./component/AboutUs";
-
-=======
-=======
->>>>>>> 64e011bbb80f504fd4605bfd78862f90c3618254
 import AboutUs from "./component/AboutUs/index";
 import Servicewedo from "./component/servicewedo/index";
 import MobileUI from "./component/serviceoffered/MobileUI/index";
 import HybridAppDevelopment from "./component/serviceoffered/HybridAppDevelopment/index";
-<<<<<<< HEAD
->>>>>>> 74621fc823ff0cc5de4048c9388ca1a7da53ed17
-=======
->>>>>>> 64e011bbb80f504fd4605bfd78862f90c3618254
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="navbar1">
-          <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/ContactUs" element={<ContactUs />} />
+    <div className="navbar1 ">
          
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Servicewedo" element={<Servicewedo />} />
+        <Route path="/MobileUI" element={<MobileUI />} />
+        <Route path="/Servicewedo/HybridAppDevelopment" element={<HybridAppDevelopment />} />
+      </Routes>
     </div>
   );
 }
--- src/component/AboutUs/index.jsx
+++ src/component/AboutUs/index.jsx
@@ -1,48 +1,16 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from 'react'
-import Leads from './Leads'
-
-=======
-=======
->>>>>>> 64e011bbb80f504fd4605bfd78862f90c3618254
 import React from "react";
 import Leads from "./Leads";
 import Story from "./story";
 import Values from "./values";
-import Footer_about from "../Homepage/footer1";
-import Clients_about from "../Homepage/clients";
+// import Footer_about from "../Homepage/footer1";
+// import Clients_about from "../Homepage/clients";
 import Topmenu from "../commonpage/Topmenu";
 import Hero from "./hero";
 import Topbar from "../Homepage/Topbar";
-<<<<<<< HEAD
->>>>>>> 74621fc823ff0cc5de4048c9388ca1a7da53ed17
-=======
->>>>>>> 64e011bbb80f504fd4605bfd78862f90c3618254
 
 const AboutUs = () => {
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <Leads/>
-=======
-      <Topbar />
-      <Topmenu />
-      <Hero />
-      <Leads />
-      <Story />
-      <Values />
-      {/* <Clients_about /> */}
-      <Footer_about />
->>>>>>> 64e011bbb80f504fd4605bfd78862f90c3618254
-    </div>
-  );
-};
-
-<<<<<<< HEAD
-export default AboutUs
-=======
       <Topbar />
       <Topmenu />
       <Hero />
@@ -56,7 +24,3 @@ export default AboutUs
 };
 
 export default AboutUs;
->>>>>>> 74621fc823ff0cc5de4048c9388ca1a7da53ed17
-=======
-export default AboutUs;
->>>>>>> 64e011bbb80f504fd4605bfd78862f90c3618254
